b4d6753409868c7333276c164bf96477c0293f53
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-03-01 01:28:43 +0800

    edited models and templates

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -29,6 +29,9 @@ class Project(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.title}"
+
     def get_absolute_url(self):
         return reverse('diyprojects:project_detail', args=[str(self.id)])
 
