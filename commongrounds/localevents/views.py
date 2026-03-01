704128428cda784d8d99130ba559856ae629723d
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-01 19:05:58 +0800

    removed user authentication

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Event
 # Create your views here.
 
@@ -14,6 +13,6 @@ class LocalEventsListView(ListView):
     context_object_name = 'events'
 
 
-class LocalEventDetailView(LoginRequiredMixin, DetailView):
+class LocalEventDetailView(DetailView):
     model = Event
     template_name = "localevent_detail.html"
