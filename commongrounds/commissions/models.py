026407883bc3a09690e6830433964b13cffa994f
ranz <edward.francis.ibera@student.ateneo.edu>
2026-03-02 04:38:30 +0800

    Updated models.py

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class CommissionType(models.Model):
     name = models.CharField(max_length=255)
@@ -23,3 +24,6 @@ class Commission(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('commission_detail', args=[self.pk])
