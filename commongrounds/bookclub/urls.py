93ee256841aecf0bce981b4a07de7c9ca4bbbf0a
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-01 11:15:27 +0800

    created views and urls

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import 
+from .views import BooksListView, BookDetailView
 
 urlpatterns = [
-
+    path('books', BooksListView.as_view(), name="books_list"),
+    path('book/<int:pk>', BookDetailView.as_view(), name="book_detail"),
 ]
 
 app_name='bookclub'
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Book
 
-# Create your views here.
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
+class BooksListView(ListView):
+    model = Book
+    template_name = "books_list.html"
