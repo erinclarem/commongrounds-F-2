f9e46f685712a1345c7011398ee60e983828d5e3
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-01 16:19:27 +0800

    Fixed urls.py in both the app and proj folder to include relevant paths, updated views
    created classes in views.py that renders the page based on the html files. added the path for each page in the app urls.py, as well as for the one in the project folder.

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import LocalEventsListView, LocalEventsDetailView
 urlpatterns = [
-    path('localevents/list', LocalEventsListView.as_view(), name='local_events_list'),
-    path('localevents/event/<int:pk>/', LocalEventsDetailView.as_view(), name='local_event_detail'),
-    ]
+    path('localevents/list', LocalEventsListView.as_view(), name='localevents_list'),
+    path('localevents/event/<int:pk>/',
+         LocalEventsDetailView.as_view(), name='localevent_detail'),
+]
 
 app_name = 'localevents'
--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import LocalEvents
 # Create your views here.
+
+
+class LocalEventsListView(ListView):
+    model = LocalEvents
+    template_name = "localevents_list.html"
+
+
+class LocalEventsDetailView(LoginRequiredMixin, DetailView):
+    model = LocalEvents
+    template_name = "localevent_detail.html"
